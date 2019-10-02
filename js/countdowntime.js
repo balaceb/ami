f9775f20f2a0894e8761eccb26e289b97353d3e6
balace ballack <erdith@gmail.com>
2019-10-02 22:42:25 +0200

    Worked on the homepage info

--- js/countdowntime.js
+++ js/countdowntime.js
@@ -1,5 +1,54 @@
-(function ($) {
+function inic(){
+	document.querySelector('.cont_modal').className = 'cont_modal cont_modal_active';
+}
+
+window.onload = function(){
+	inic();
+}
+	
+function getTimeRemaining(endtime) { 
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor((t / 1000) % 60);
+	var minutes = Math.floor((t / 1000 / 60) % 60);
+	var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	return {
+	  'total': t,
+	  'days': days,
+	  'hours': hours,
+	  'minutes': minutes,
+	  'seconds': seconds
+	};
+}
+		  
+function initializeClock(endtime) { 
+	var daysSpan = $(obj).find('.days');
+	var hoursSpan = $(obj).find('.hours');
+	var minutesSpan = $(obj).find('.minutes');
+	var secondsSpan = $(obj).find('.seconds');
+
+	function updateClock() { 
+	  var t = getTimeRemaining(endtime);
+
+	  daysSpan.html(t.days);
+	  hoursSpan.html(('0' + t.hours).slice(-2));
+	  minutesSpan.html(('0' + t.minutes).slice(-2));
+	  secondsSpan.html(('0' + t.seconds).slice(-2))
+
+	  if (t.total <= 0) {
+		clearInterval(timeinterval);
+	  }
+	}
+
+	updateClock();
+	var timeinterval = setInterval(updateClock, 1000);
+  }
+	
+/*(function ($) {
     "use strict";
+
+	
+
 
     $.fn.extend({ 
 
@@ -89,4 +138,4 @@
 
     
 
-})(jQuery);
+})(jQuery);*/
